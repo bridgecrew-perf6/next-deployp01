fc8f2a95d7401e48ce168fc8d0d4db152de3f8d2
oscarjeremias <oscarjeremias3@gmail.com>
2022-02-11 19:55:18 +0100

    add 01

---
+++ pages/hello.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export function Hello() {
+  return (
+  <h1>hello</h1>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image from 'next/image'
+import { Hello } from './hello.tsx'
+import foto from './foto.png'
+function HomePage () {
+  return (
+  <>
+  <h1>hello world</h1>
+  <Image
+  src={foto} 
+  width={250}
+  height={400}
+  placeholder="blur"
+  />
+  <Hello />
+  <style jsx>{`
+  h1 {
+    text-align: center;
+  }
+  `}</style>
+  </>
+  )
+}
+export default HomePage
